92474d68bd6d215f714d4808e311d5fd390e6218
Katherine Guarnizo <guarnizo.katherine.5@gmail.com>
2023-07-27 14:44:25 -0400

    Working on models

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
+# from main.models import Book, BookSerializer....need to update to reflect our model
 
 # Create your views here.
+# class BookList(APIView): ....need to update to reflect our model
+    def get(self, request):
+        # books = Book.objects.all()...need 
+        serializer = BookSerializer(books, many=True)
+        return JsonResponse(serializer.data, safe=False)
