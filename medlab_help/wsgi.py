5f4e15c3cbc2822c39cb443e28249335bcdf3250
Katherine Guarnizo <guarnizo.katherine.5@gmail.com>
2023-07-27 13:31:41 -0400

    Create directories

---
+++ medlab_help/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for medlab_help project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medlab_help.settings')
+
+application = get_wsgi_application()
