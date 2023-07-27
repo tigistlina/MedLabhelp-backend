d03216dd9b7ffbd31e1e26fa46639f763a324763
Tigist Woldearegai <tigistlina@gmail.com>
2023-07-27 18:55:46 -0500

    creates serializer.py

--- app/views.py
+++ app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
-# from main.models import Book, BookSerializer....need to update to reflect our model
-
+from app.models import Test , TestSerializer
 # Create your views here.
-# class BookList(APIView): ....need to update to reflect our model
+class TestList(APIView):
     def get(self, request):
-        # books = Book.objects.all()...need 
-        serializer = BookSerializer(books, many=True)
+        tests = Test.objects.all()
+        serializer = TestSerializer(tests, many=True)
         return JsonResponse(serializer.data, safe=False)
+    
+class TestDetail(APIView):
+    def get(self,request,id):
+        pass
