034b546c8012d42023ee1c359f05865f4bf66025
Jasmin Worthy <jasminworthy@gmail.com>
2023-07-27 19:42:19 -0500

    resaved and pushed

--- app/serializers.py
+++ app/serializers.py
@@ -4,4 +4,11 @@ from .models import Test
 class TestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Test
-        fields = ['id','']
+        fields = ['id', 'panel_id', 'name', 'description', 'info_url', 'normal_reference', 'unit_of_measure']
+
+
+
+
+
+
+        
--- medlab_help/urls.py
+++ medlab_help/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.tests import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tests/', views.testlist)
+    path('tests/<int:id>', views.TestDetail)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
