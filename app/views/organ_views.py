2498134f82e72bf5fba8f1b22a14713eb31350cf
Tigist Woldearegai <tigistlina@gmail.com>
2023-07-29 16:06:43 -0500

    creats views_helper.py function to validate id

--- app/views/organ_views.py
+++ app/views/organ_views.py
@@ -4,7 +4,7 @@ from ..serializers import OrganSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-# from .views_helper import validate_model
+from .views_helper import validate_model
 
 @api_view(['GET'])
 def OrganList(request, format=None):
@@ -16,12 +16,13 @@ def OrganList(request, format=None):
 
 @api_view(['GET'])
 def OrganDetail(request, id, format=None):
+    organs = validate_model(Organ, id)
 
-    try:
-        organs = Organ.objects.get(pk=id)
-    except Organ.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    if not organs:
+        return JsonResponse({'error': f"Organ with id {id} not found"}, status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = OrganSerializer(organs)
         return Response(serializer.data)
+    else:
+        return Response({'error': 'Method Not Allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
--- app/views/views_helper.py
+++ app/views/views_helper.py
@@ -5,13 +5,13 @@ from django.http import JsonResponse
 
 def validate_model(cls,model_id):
     try:
-        model_id = int(model_id)
-    except ObjectDoesNotExist:
-        return JsonResponse(f"Test with id {model_id} does not exist.",safe=False)
-        
-    model = cls.query.get(model_id)
+        int_model_id = int(model_id)
+    except ValueError:
+        return JsonResponse(f"{cls.__name__} with id {model_id} does not exist.",safe=False)
 
-    if not model:
-        return JsonResponse({"message":f"{cls.__name__} {model_id} not found"}, 404)
+    try:
+        model = cls.objects.get(pk=int_model_id)
+    except cls.DoesNotExist:
+        return JsonResponse({"message":f"{cls.__name__} {int_model_id} not found"}, 404)
 
     return model
